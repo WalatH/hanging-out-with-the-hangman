044d0796a801ee4cc1e87568fd479a8651a7e686
Mikail Sola <87417382+MikailSola@users.noreply.github.com>
2022-04-22 07:57:19 +0000

    Random number picker works

---
+++ main.py
@@ -0,0 +1,5 @@
+import random
+print("Howdy partner, welcome to the gallows.")
+
+words = ["Informatica", "Informatiekunde", "Spelletje", "Aardigheidje", "Scholier", "Fotografie", "Waardebepaling", "Specialiteit", "Verzekering", "Universiteit", "Heesterpark"]
+print(random.choice(words))
