bdb6e080c1a5bb2333b3578d14704a45985aea70
WalatH <93122449+WalatH@users.noreply.github.com>
2022-05-13 07:27:54 +0000

    Letters in word

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ print("Howdy partner, welcome to the gallows.")
 
 words = ["Informatica", "Informatiekunde", "Spelletje", "Aardigheidje", "Scholier", "Fotografie", "Waardebepaling", "Specialiteit", "Verzekering", "Universiteit", "Heesterpark"]
 
-letters = len(words)
-print(random.choice(words)) 
-print(letters)
-
-
-print("MONEY MONEY")
-
+rng = (random.choice(words))
+number_of_letters = len(rng) - rng.count(" ")
+print(number_of_letters)
+print(rng)
