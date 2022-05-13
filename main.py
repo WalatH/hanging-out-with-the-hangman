44a540024f29b10dc7825dc2d14d3284b9993aa1
Mikail Sola <87417382+MikailSola@users.noreply.github.com>
2022-05-13 07:07:24 +0000

    print hehehehaw

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ import random
 print("Howdy partner, welcome to the gallows.")
 
 words = ["Informatica", "Informatiekunde", "Spelletje", "Aardigheidje", "Scholier", "Fotografie", "Waardebepaling", "Specialiteit", "Verzekering", "Universiteit", "Heesterpark"]
+
+letters = len(words)
 print(random.choice(words)) 
-print("GRRRRRRR")
+print(letters)
+
+print("Hehehehaw")
