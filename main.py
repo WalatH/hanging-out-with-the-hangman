f578303f5d24724e0d5afa3ccd2dcd02193641e2
WalatH <93122449+WalatH@users.noreply.github.com>
2022-05-13 08:03:28 +0000

    For loop guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-print("Howdy partner, welcome to the gallows.")
+print("Howdy partner, welcome to the gallows. You tryin' to hang out with me?")
 
 words = ["Informatica", "Informatiekunde", "Spelletje", "Aardigheidje", "Scholier", "Fotografie", "Waardebepaling", "Specialiteit", "Verzekering", "Universiteit", "Heesterpark"]
 
@@ -10,5 +10,12 @@ space = len(rng)
 underscore = ("_ " * space)
 print(underscore)
 
+for i in range(1, 9):
+    letter = input("Guess a letter ")
+    if letter in rng:
+        print ("Correct",letter)
+    else:
+        print ("Incorrect", " ",letter)
+      
 print(number_of_letters)
 print(rng)
