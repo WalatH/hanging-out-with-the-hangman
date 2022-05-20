67c969afdfd9380a3d9a268794e8bda8de839f92
WalatH <93122449+WalatH@users.noreply.github.com>
2022-05-20 07:06:45 +0000

    for loop kinda works

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import random
+import os
 print("Howdy partner, welcome to the gallows. You tryin' to hang out with me?")
 
 words = ["Informatica", "Informatiekunde", "Spelletje", "Aardigheidje", "Scholier", "Fotografie", "Waardebepaling", "Specialiteit", "Verzekering", "Universiteit", "Heesterpark"]
 
 rng = (random.choice(words))
-number_of_letters = len(rng) - rng.count(" ")
+number_of_letters = len(rng)
 
 space = len(rng)
 underscore = ("_ " * space)
 print(underscore)
 
 for i in range(1, 9):
-    letter = input("Guess a letter ")
+    letter = input("Guess a letter")
     if letter in rng:
         print ("Correct",letter)
     else:
-        print ("Incorrect", " ",letter)
+        print ("Incorrect",letter)
+
       
 print(number_of_letters)
 print(rng)
